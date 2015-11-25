a199be93bc734dd0d3917441f781c12becc5610d
develask <develascomikel@gmail.com>
2015-11-25 15:46:04 +0100

    Primer commit

---
+++ Kmeans/src/src/Kmeans.java
@@ -0,0 +1,194 @@
+package src;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import distance.Minkowski;
+import weka.clusterers.Clusterer;
+import weka.core.Attribute;
+import weka.core.Capabilities;
+import weka.core.DenseInstance;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.Remove;
+import weka.filters.unsupervised.attribute.RemoveType;
+
+public class Kmeans implements Clusterer{
+	
+	private int numCentroides;
+	private Instance[] centroides;
+	private Instances instancias;
+	private Minkowski m;
+	private ArrayList<Instance>[] grupos;
+	
+	public Kmeans(int centroide, int minkowski){
+		this.numCentroides = centroide;
+		this.m = new Minkowski(minkowski);
+	}
+	private boolean compararCentroides(Instance[] centroides1, Instance[] centroides2){
+		if (centroides1 == null || centroides2 == null ) return false;
+		Instance instance;
+		Instance instance2;
+		for (int i = 0; i < centroides1.length; i++) {
+			instance = centroides1[i];
+			instance2 = centroides2[i];
+			if (instance == null || instance2 == null ) return false;
+			for (int j = 0; j < instance.numAttributes(); j++) {
+				if (instance.value(j) != instance2.value(j)) return false;
+			}
+		}
+		return true;
+	}
+	@Override
+	public void buildClusterer(Instances arg0) throws Exception {
+		RemoveType rm = new RemoveType();
+		rm.setOptions(new String[]{"-T", "numeric", "-V"});
+		rm.setInputFormat(arg0);
+		this.instancias = Filter.useFilter(arg0, rm);
+		this.centroides = new Instance[this.numCentroides];
+		Instance[] centroidesTmp = new Instance[this.numCentroides];
+		this.buscarPrimerosCentroides(centroidesTmp);
+		grupos = new ArrayList[this.numCentroides];
+		int buelta = 0;
+		while(!this.compararCentroides(this.centroides, centroidesTmp)){
+			for (int i = 0; i < this.grupos.length; i++) {
+				this.grupos[i] = new ArrayList<Instance>();
+				
+			}
+			this.centroides=centroidesTmp;
+			for (Instance is : this.instancias) {
+				this.grupos[this.clusterInstance(is)].add(is);
+			}
+			centroidesTmp = this.nuevosCentroides();
+			System.out.println(buelta++ + " | " + centroidesTmp[0] + " , " + centroidesTmp[1] + " , " +centroidesTmp[2]);
+			System.out.println();
+		}
+	}
+	
+	private Instance[] nuevosCentroides() {
+		Instance[] nuevos = new Instance[this.numCentroides];
+		for (int c=0; c<this.grupos.length; c++) {
+			ArrayList<Instance> grupo = grupos[c];
+			double[] attr = new double[this.instancias.numAttributes()];
+			for (Instance ins : grupo) {
+				for (int i = 0; i < attr.length; i++) {
+					attr[i]+=ins.value(i);
+				}
+			}
+			Instance ins = new DenseInstance(attr.length);
+			for (int i = 0; i < attr.length; i++) {
+				attr[i]/=grupo.size();
+				ins.setValue(i, attr[i]);
+			}
+			nuevos[c] = ins;
+		}
+		return nuevos;
+	}
+	
+	private void buscarPrimerosCentroides(Instance[] centroidesTmp){
+		int r;
+		ArrayList<Integer> nums = new ArrayList<Integer>();
+		for (int i = 0; i < this.numCentroides; i++) {
+			do{
+				r = (int) Math.floor(Math.random() * this.instancias.numInstances());
+			}while(nums.contains(r));
+			centroidesTmp[i] = this.instancias.get(r);
+		}
+	}
+
+	@Override
+	public int clusterInstance(Instance arg0) throws Exception {
+		double[] distancias = new double[this.numCentroides];
+		for (int i = 0; i < distancias.length; i++) {
+			distancias[i] = this.m.calcularDistancia(arg0, this.centroides[i]);
+		}
+		double min = Double.MAX_VALUE;
+		int pos = -1;
+		for (int i=0; i<distancias.length; i++) {
+			double d = distancias[i];
+			if (min > d){
+				min = d;
+				pos = i;
+			}
+		}
+		return pos;
+	}
+	
+	public double SSE(){
+		double suma=0.0;
+		for (int i=0; i<this.grupos.length; i++) {
+			ArrayList<Instance> grupo = this.grupos[i];
+			for (Instance instance : grupo) {
+				suma += Math.pow(this.m.calcularDistancia(instance, this.centroides[i]), 2);
+			}
+		}
+		return suma;
+	}
+	
+	public double silhouette(){
+		double sumaA;
+		double sumaB;
+		double si=0.0;
+		for (int i=0; i<this.grupos.length; i++) {
+			ArrayList<Instance> grupo = this.grupos[i];
+			for (Instance instance : grupo) {
+				sumaA = this.calcularDistanciaMediaGrupo(instance, grupo, true);
+				sumaB = this.calcularDistanciaMinimaMediaOtrosGrupo(instance, grupo);
+				si += (sumaB-sumaA)/(sumaA<sumaB?sumaB:sumaA);
+			}
+		}
+		return si/this.instancias.size();
+	}
+	private double calcularDistanciaMediaGrupo(Instance ins, ArrayList<Instance> grupo, boolean tuyo){
+		double suma=0.0;
+		for (Instance instance : grupo) {
+			suma+=this.m.calcularDistancia(ins, instance);
+		}
+		return suma/(grupo.size()-(tuyo?1:0));
+	}
+	
+	private double calcularDistanciaMinimaMediaOtrosGrupo(Instance ins, ArrayList<Instance> grupo){
+		double min = Double.MAX_VALUE;
+		double tmp;
+		for (ArrayList<Instance> grup : this.grupos) {
+			if (grupo != grup){
+				tmp = this.calcularDistanciaMediaGrupo(ins, grup, false);
+				if (tmp<min)min=tmp;
+			}
+		}
+		return min;
+	}
+
+	@Override
+	public double[] distributionForInstance(Instance arg0) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Capabilities getCapabilities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int numberOfClusters() throws Exception {
+		return this.numCentroides;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		Instances ins = Lector.getLector().leerInstancias("iris.arff");
+		Remove rm = new Remove();
+		rm.setAttributeIndicesArray(new int[]{ins.classIndex()});
+		rm.setInputFormat(ins);
+		ins = Filter.useFilter(ins, rm);
+		Kmeans k = new Kmeans(3,2);
+		k.buildClusterer(ins);
+		System.out.println("Ya se ha clusterizado.");
+		System.out.println("SSE: " + k.SSE());
+		System.out.println("Silhouette: " + k.silhouette());
+	}
+
+}
