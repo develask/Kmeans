e99dfc24bcb6019ae30befa46a1281a4e2de1c6c
develask <develascomikel@gmail.com>
2015-11-26 15:17:06 +0100

    Probando cosas

--- Kmeans/src/main/Ejecutador.java
+++ Kmeans/src/main/Ejecutador.java
@@ -23,43 +23,42 @@ public class Ejecutador {
 		Instances ins2 = Filter.useFilter(ins, rm);
 		
 		//parametros del cluster
-		int k = 3;
-		long tiempo= 1000 * 20;
+		int k = 2;
+		long tiempo= 1000 * 2;
 		
 		// Generamos el Clusterizador
 		Kmeans kmeans = new Kmeans(k,2);
 		kmeans.setTimeOut(tiempo);
 		
-		ArrayList<Object[]> results = new ArrayList<Object[]>();
-		double num;
-		boolean enc;
-		for (int i = 0; i < 50; i++) {
-			System.out.print(i + " ");
+//		ArrayList<Object[]> results = new ArrayList<Object[]>();
+//		double num;
+//		boolean enc;
+//		for (int i = 0; i < 500; i++) {
+//			System.out.print(i + " ");
 			kmeans.buildClusterer(ins2);
-			num = kmeans.SSE();
-			enc = false;
-			for (Object[] objects : results) {
-				if (objects[0].equals(num)){
-					enc = true;
-					break;
-				}
-			}
-			if (!enc){
-				results.add(new Object[]{num,kmeans.distributionForCluster()});
-			}
-		}
-		System.out.println();
-		for (Object[] objects : results) {
-			System.out.println("----------------");
-			System.out.println("Silhouette: " + objects[0]);
-			int i = 0;
-			for (ArrayList<Instance> instances : (ArrayList<Instance>[])objects[1]) {
-				System.out.println("\t- Grupo "+ ++i + ":\t" + instances.size() + " instancias.");
-			}
-		}
+//			num = kmeans.SSE();
+//			enc = false;
+//			for (Object[] objects : results) {
+//				if (objects[0].equals(num)){
+//					enc = true;
+//					break;
+//				}
+//			}
+//			if (!enc){
+//				results.add(new Object[]{num,kmeans.distributionForCluster()});
+//			}
+//		}
+//		System.out.println();
+//		for (Object[] objects : results) {
+//			System.out.println("----------------");
+//			System.out.println("Silhouette: " + objects[0]);
+//			int i = 0;
+//			for (ArrayList<Instance> instances : (ArrayList<Instance>[])objects[1]) {
+//				System.out.println("\t- Grupo "+ ++i + ":\t" + instances.size() + " instancias.");
+//			}
+//		}
 		
 		
-		/*
 		//Clasificar
 		int[][] grupos = new int[ins.classAttribute().numValues()][k];
 		for (int i = 0; i < grupos.length; i++) {
@@ -81,28 +80,28 @@ public class Ejecutador {
 //		}
 //		result+="\n";
 		result += "\t";
-		for (int i = 0; i < grupos.length; i++) {
+		for (int i = 0; i < grupos[0].length; i++) {
 			result +=("  "+(char)(97+i) + "\t");
 		}
 		result+="   <-- Clusterizado como\n";
 		result += "\t";
-		for (int i = 0; i < grupos.length; i++) {
+		for (int i = 0; i < grupos[0].length; i++) {
 			result +=("+-------");
 		}
 		result+="+\n";
 		Enumeration<Object> clases = ins.classAttribute().enumerateValues();
 		for (int f = 0; f < grupos.length; f++) {
 			result += "\t";
-			for (int i = 0; i < grupos.length; i++) {
+			for (int i = 0; i < grupos[0].length; i++) {
 				result +=("| "+grupos[f][i] + "\t");
 			}
 			result+="| "+clases.nextElement()+"\n";
 			result += "\t";
-			for (int i = 0; i < grupos.length; i++) {
+			for (int i = 0; i < grupos[0].length; i++) {
 				result +=("+-------");
 			}
 			result+="+\n";
 		}
-		System.out.println(result);*/
+		System.out.println(result);
 	}
 }
